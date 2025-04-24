01f36aeca1b0c0166b2eba232b99389ac402daac
Páblisson Araújo <pablissonaraujo@gmail.com>
2025-04-24 19:57:14 +0000

    criando a model e a migration

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+	protected $fillable = [
+		'id',
+		'name',
+		'description',
+		'is_active'
+	];
+
+	public $incrementing = false;
+
+	protected $casts = [
+		'id' => 'string',
+		'is_active' => 'boolean',
+		'deleted_at' => 'datetime',
+	];
+}
