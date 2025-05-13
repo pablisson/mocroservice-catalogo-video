b1c0965e2777dbbee4e9c903c9cd1966da4099c1
Páblisson Araújo <pablissonaraujo@gmail.com>
2025-05-13 11:38:23 +0000

    feat: criação do arquivo de teste unitário

---
+++ tests/Unit/Domain/Entity/GenreUnitTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit\Domain\Entity;
+
+use PHPUnit\Framework\TestCase;
+use Core\Domain\Entity\Genre;
+
+class GenreUnitTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_attributes(): void
+    {
+        $genre = new Genre();
+    }
+}
